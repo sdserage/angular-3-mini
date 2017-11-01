b4989d9259176ac267858f24a0fa8d4d75715dd6
Scott Serage <xttocsurge51@gmail.com>
2017-11-01 14:10:56 -0600

    Completed step 3

---
+++ app/cars/cars.component.js
@@ -0,0 +1,11 @@
+angular.module('forWheels',[]).component('cars',{
+  templateUrl: 'app/cars/cars.template.html',
+  controllerAs: 'carsCtrl',
+  controller: function(carsSrvc){
+    this.cars = carsSrvc.cars;
+    this.buyCar = id => {
+      carsSrvc.buyCar(id);
+      this.cars = carsSrvc.cars;
+    }
+  }
+});
